d4244e193fccb3a3095202da362399c33ae36dba
phkundi <p.kundratitz@hotmail.com>
2024-02-26 18:02:09 +0100

    added nuxt3 example

--- examples/nuxt3/plugins/survser.js
+++ examples/nuxt3/plugins/survser.js
@@ -1,9 +1,24 @@
+// plugins/survser.js
+
 import { defineNuxtPlugin } from "#app";
 import _survser from "survser-js";
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (process.client) {
-    _survser("setProject", "65cffe99bd9ad842f35f25a7");
-    _survser("setAPIKey", "c62bc297-42a1-4d6f-8039-8713e6a8c532");
+    // Replace with actual logic to retrieve user data
+    const user = {
+      id: "12345",
+      email: "john@doe.com",
+      name: "John Doe",
+    };
+
+    // Replace with actual project ID and API key
+    _survser("setProject", "<PROJECT_ID>");
+    _survser("setAPIKey", "<API_KEY>");
+    _survser("identifyUser", {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    });
   }
 });
